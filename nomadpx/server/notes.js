4f117660ddc55aa1a5919cdac6bdb51e40bbb023
ruijadom <ruijadomingues@gmail.com>
2016-10-28 12:24:58 +0100

    server

---
+++ nomadpx/server/notes.js
@@ -0,0 +1,39 @@
+function isAdmin(userId) {
+  return userId === 'YTBKpxPLzEoFjsGy6';  
+}
+
+Meteor.startup(function () {
+
+  Meteor.publish('notes', function () {
+    return Notes.find(
+      { },
+      {
+        sort: { timestamp: -1 }
+      }
+    )
+  });
+
+  // security
+  Notes.allow({
+    insert: function (userId, doc) {
+      // anybody can insert
+      return true;
+    },
+    update: function (userId, doc, fields, modifier) {
+      return userId && doc.userId === userId || isAdmin(userId);
+    },
+    remove: function (userId, doc) {
+      // can only remove your own notes
+      return userId && doc.userId === userId || isAdmin(userId);
+    }
+  });
+
+  // Global API configuration
+  var Api = new Restivus({
+    prettyJson: true
+  });
+
+  // Generates: GET, POST on /api/notes and GET, PUT, DELETE on
+  // /api/notes/:id for the Notes collection
+  Api.addCollection(Notes);
+});
